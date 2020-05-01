31c99cd7de51b041d41bfee93f7e1cd833e92f04
Юлия <yanju@tut.by>
2020-05-02 00:42:00 +0300

    2 tests added

--- src/test/java/BMICalculatorTest.java
+++ src/test/java/BMICalculatorTest.java
@@ -3,16 +3,47 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
+
 public class BMICalculatorTest {
 
-    @Test
-    public void categoryTest() {
+    @Test(description = "Check Normal Weight Category")
+    public void categoryTest1() {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-
         WebDriver browser = new ChromeDriver();
         browser.get("https://healthunify.com/bmicalculator/");
         browser.findElement(By.name("wg")).sendKeys("82");
         browser.findElement(By.name("ht")).sendKeys("185");
+        browser.findElement(By.name("cc")).click();
+        String category = browser.findElement(By.name("desc")).getAttribute("value");
+        assertEquals(category, "Your category is Normal", "Категория верная");
+        browser.quit();
+    }
+
+    @Test(description = "Check Obese Weight Category")
+    public void categoryTest2() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://healthunify.com/bmicalculator/");
+        browser.findElement(By.name("wg")).sendKeys("140");
+        browser.findElement(By.name("ht")).sendKeys("185");
+        browser.findElement(By.name("cc")).click();
+        String category = browser.findElement(By.name("desc")).getAttribute("value");
+        assertEquals(category, "Your category is Obese", "Категория верная");
+        browser.quit();
+    }
+
+
+    @Test(description = "Check Underweight Category")
+    public void categoryTest3() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://healthunify.com/bmicalculator/");
+        browser.findElement(By.name("wg")).sendKeys("30");
+        browser.findElement(By.name("ht")).sendKeys("185");
+        browser.findElement(By.name("cc")).click();
+        String category = browser.findElement(By.name("desc")).getAttribute("value");
+        assertEquals(category, "Your category is Starvation", "Категория верная");
         browser.quit();
     }
 }
