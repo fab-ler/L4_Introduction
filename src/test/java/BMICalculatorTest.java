45193fa721ba4be2f09a9883df2651cb1da8012d
Юлия <yanju@tut.by>
2020-05-02 22:23:40 +0300

    add examples for lesson 7 (how to use chrome options and other WebDriver actions)

--- src/test/java/BMICalculatorTest.java
+++ src/test/java/BMICalculatorTest.java
@@ -1,8 +1,11 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.assertEquals;
 
@@ -51,11 +54,32 @@ public class BMICalculatorTest {
     @Test
     public void lesson6() {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        //further refer to https://chromedriver.chromium.org/capabilities
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
+        //       options.addArguments("--headless");
         options.addArguments("--start-maximized");
-        WebDriver browser = new ChromeDriver();
-        browser.get("https://tut.by")
+        WebDriver chrome = new ChromeDriver();
+        chrome.manage().window().maximize();
+        chrome.manage().window().setSize(new Dimension(1024, 768));
+        //поиск элементов будет ожидать МАКСИМУМ 10 секунд до появления элемента, если элемент не появился, текст упадет с NoSuchElementException
+        chrome.get("http://the-internet.herokuapp.com/iframe");
+        chrome.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //пример навигации
+//        chrome.navigate().refresh();
+//        chrome.navigate().back();
+        //getWindowHandle method
+//        String currentWindow = chrome.getWindowHandle();
+        //frame and alerts require switchTo
+        //сначала переключаемся на этот элемент, потом пытаемся найти тег <p>  с нужным текстом (не забыть обратно вернуться)
+        chrome.switchTo().frame(chrome.findElement(By.id("mce_0_ifr")));
+        chrome.findElement(By.xpath("//p[contains(text(),'Your content goes here.')]")).sendKeys("My Text");
+        //вернуться назад из iframe
+        chrome.switchTo().defaultContent();
+//        chrome.switchTo().window(currentWindow);
+        //работа с алертами  - они бывают трех типов (есть которые можно закрыть
 
+        //chrome.switchTo().alert().
+
+        chrome.quit();
     }
 }
